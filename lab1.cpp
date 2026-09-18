492685f105a840f6cb2bf2209fe11e277ddb1d2e
lordelbardooo <tenakor.bron@heig-vd.ch>
2026-09-18 13:13:07 +0200

    v1

---
+++ lab1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lordelbardooo on 9/18/26.
+//
+
+#include "lab1.h"
---
+++ lab1.h
@@ -0,0 +1,18 @@
+main (a,b) // methode principale
+// verification saisie
+si b != a+-1:
+    print(error)
+    return
+
+c,d = 2,1
+
+// boucle algo
+while c >= a and d<=b{
+    if (c>d):
+        d+2
+    if (c<d):
+        c+2
+    print (c/d)
+}
+
+
